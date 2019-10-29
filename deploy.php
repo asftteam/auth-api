d4d156fce48f47f56fe70ffa4f973e2a876cd503
Alex Plekhanov <alex@plekhanov.us>
2019-10-30 00:43:28 +0100

    Added deploy

---
+++ deploy.php
@@ -0,0 +1,49 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'my_project');
+
+// Project repository
+set('repository', 'git@github.com:asftteam/auth-api.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server 
+add('writable_dirs', []);
+set('allow_anonymous_stats', false);
+
+// Hosts
+inventory('hosts.yml');
+
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
+
+task('php-fpm-reload', function () {
+    run('sudo service php7.3-fpm reload');
+});
+
+after('deploy:failed', 'deploy:unlock');
+
+after('artisan:config:cache', 'artisan:route:cache');
+
+after('deploy:symlink', 'php-fpm-reload');
+
+after('rollback', 'php-fpm-reload');
